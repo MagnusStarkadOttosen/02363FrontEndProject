1ee7d8b81d0b980afe5d3ad6ee3d325adb1cf452
Magnus Ottosen <magnus@ottosen.dk>
2024-02-26 15:31:06 +0100

    Removed unneeded imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { Component, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ItemList from './components/ItemList';
 
