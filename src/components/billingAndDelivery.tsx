450ae87af3af1a550d93d1faa3a69dd89fef041a
Magnus Ottosen <magnus@ottosen.dk>
2024-03-10 21:44:09 +0100

    Validated zip

--- src/components/billingAndDelivery.tsx
+++ src/components/billingAndDelivery.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/BillingAndDelivery.css';
 
 const BillingAndDelivery: React.FC = () => {
@@ -20,29 +20,17 @@ const BillingAndDelivery: React.FC = () => {
             ...prevState,
             [name]: value,
         }));
-
-        if(name === "orderZip"){
-            validateZip(value);
-        }
-
-        if (name === 'orderCountry' && formState.orderZip) {
-            validateZip(formState.orderZip);
-        }
     };
 
     const [zipValid, setZipValid] = useState(true); //For zip
 
-    const validateZip = async (zip: string) => {
-        if (formState.orderCountry === "DK" && zip.length === 4) {
+    const validateZip = async (zip: string, country: string) => {
+        if (country === "DK") {
             try {
                 const response = await fetch(`https://api.dataforsyningen.dk/postnumre/${zip}`);
                 if (response.ok) {
                     const data = await response.json();
-                    if (data.nr) {
-                        setZipValid(true);
-                    } else {
-                        setZipValid(false);
-                    }
+                    setZipValid(data.length > 0);
                 } else {
                     setZipValid(false);
                 }
@@ -51,7 +39,7 @@ const BillingAndDelivery: React.FC = () => {
                 setZipValid(false);
             }
         } else {
-            setZipValid(true);
+            setZipValid(true); //If not denmark assume zip is correct
         }
     };
 
@@ -60,43 +48,43 @@ const BillingAndDelivery: React.FC = () => {
         validateZip(formState.orderZip, formState.orderCountry);
     }, [formState.orderCountry, formState.orderZip]);
 
-    return(
+    return (
         <div className='form-wrapper'>
             <div className="row">
                 <div>
                     <label className="control-label" htmlFor="orderFirstName">First Name</label>
-                    <input id="orderFirstName" className='form-control'type='text' name='orderFirstName' value={formState.orderFirstName} onChange={handleInputChange}></input>
+                    <input id="orderFirstName" className='form-control' type='text' name='orderFirstName' value={formState.orderFirstName} onChange={handleInputChange}></input>
                 </div>
                 <div>
                     <label className="control-label" htmlFor="orderLastName">Last Name</label>
-                    <input id="orderLastName" className='form-control'type='text' name='orderLastName' value={formState.orderLastName} onChange={handleInputChange}></input>
+                    <input id="orderLastName" className='form-control' type='text' name='orderLastName' value={formState.orderLastName} onChange={handleInputChange}></input>
                 </div>
             </div>
             <div className="row">
                 <div>
                     <label className="control-label" htmlFor="orderPhone">Phone</label>
-                    <input id="orderPhone" className='form-control'type='text' name='orderPhone' value={formState.orderPhone} onChange={handleInputChange}></input>
+                    <input id="orderPhone" className='form-control' type='text' name='orderPhone' value={formState.orderPhone} onChange={handleInputChange}></input>
                 </div>
                 <div>
                     <label className="control-label" htmlFor="orderEmail">Email</label>
-                    <input id="orderEmail" className='form-control'type='text' name='orderEmail' value={formState.orderEmail} onChange={handleInputChange}></input>
+                    <input id="orderEmail" className='form-control' type='text' name='orderEmail' value={formState.orderEmail} onChange={handleInputChange}></input>
                 </div>
             </div>
             <div className='row'>
                 <div>
                     <label className="control-label" htmlFor="orderAddress">Address</label>
-                    <input id="orderAddress" className='form-control'type='text' name='orderAddress' value={formState.orderAddress} onChange={handleInputChange}></input>
+                    <input id="orderAddress" className='form-control' type='text' name='orderAddress' value={formState.orderAddress} onChange={handleInputChange}></input>
                 </div>
             </div>
             <div className="row">
                 <div>
                     <label className="control-label" htmlFor="orderZip">Zip code</label>
-                    <input id="orderZip" className='form-control'type='text' name='orderZip' value={formState.orderZip} onChange={handleInputChange}></input>
+                    <input id="orderZip" className='form-control' type='text' name='orderZip' value={formState.orderZip} onChange={handleInputChange}></input>
                     {!zipValid && <div className="invalid-feedback">Invalid ZIP code for Denmark.</div>}
                 </div>
                 <div>
                     <label className="control-label" htmlFor="orderCity">City</label>
-                    <input id="orderCity" className='form-control'type='text' name='orderCity' value={formState.orderCity} onChange={handleInputChange}></input>
+                    <input id="orderCity" className='form-control' type='text' name='orderCity' value={formState.orderCity} onChange={handleInputChange}></input>
                 </div>
             </div>
             <div>
