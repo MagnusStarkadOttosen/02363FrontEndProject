68673e200e2e1b0dcecf5d98aa95cd8ffc6677b1
Magnus Ottosen <magnus@ottosen.dk>
2024-03-11 10:56:02 +0100

    Fixed bunch of small errors

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -1,23 +1,20 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Item } from '../types/Items';
-import Button from 'react-bootstrap/Button';
+import { Button } from 'react-bootstrap';
 
 //Interface with the items properties.
-interface ItemProps{
+interface ItemProps {
     item: Item; //The details of what the item contains.
     onRemove: (id: string) => void; //Function thats called when removing items.
     onQuantityChange: (id: string, subtotal: number) => void; //Function thats called when updating quantity.
 }
-
-    
-    
 
 const ItemComponent: React.FC<ItemProps> = ({ item, onRemove, onQuantityChange }) => {
     //Tracking quantity defaults to 1.
     const [quantity, setQuantity] = useState(1);
     const increaseQuantity = () => setQuantity(prev => prev + 1);
     const decreaseQuantity = () => setQuantity(prev => prev > 0 ? prev - 1 : 0);
-    const [gift, setGift] = useState(0);
+    const [gift, setGift] = useState(false);
     //Calculates the subtotal based on quantity.
     let subTotal = item.price * quantity;
     let totalDiscount = 0;
@@ -30,49 +27,43 @@ const ItemComponent: React.FC<ItemProps> = ({ item, onRemove, onQuantityChange }
     }, [quantity]); //"quantity" in [] means this effect runs then "quantity" changes.
 
     //calculate discount per item
-    if(item.amount >= item.rebateQuantity){
+    if (item.amount >= item.rebateQuantity) {
         totalDiscount = subTotal;
         const discountInDecimal = item.rebatePercent / 100;
         subTotal = subTotal * (1 - discountInDecimal);
         totalDiscount = totalDiscount - subTotal;
     }
-    
+
     //What is shown for each item.
 
-    return(
-                <div>
-                    <img src={item.imageSrc} alt ="Image" width="100" height="100"/>
-                    <span>
-                    {item.name}
-                    </span>
-                    <br>
-                    <label>Gift wrap <input type="checkbox" value={gift} onChange={(e) => setGift(Number(e.target.value))}/></label>
-                    </br>
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <button onClick={decreaseQuantity}>-</button>
-                        <input //The input for quantity.
-                            type="number"
-                            value={quantity} //Input value bound to components quantity.
-                            onChange={(e) => setQuantity(Number(e.target.value)||0)} //Update quantity state if quantity is changed.
-                            style={{ textAlign: 'center', width: '30px', margin: '0 5px' }}
-                            min="1" //Set the minimun number to 1.
-                        />
-                        <button onClick={increaseQuantity}>+</button>
-                    </div>
-                    <br>
-                    {" " + item.rebatePercent + "% discount for " + item.rebateQuantity + "pcs, "} {/*Button to remove the item.*/}
-                    </br>
-                    <span>
-                    {" " + subTotal.toFixed(2)} {/*Display the subtotal.*/}
-                    </span>
-                    <br>
-                    {"( " + totalDiscount.toFixed(2)+")" } {/*Display the subtotal.*/}
-                    </br>
-                    <span>
-                    <Button onClick={()=> onRemove(item.id)} variant="danger">🗑️</Button> {/*Button to remove the item.*/} 
-                    </span>
-                   
-             </div>
-            )}
+    return (
+        <div>
+            <img src={item.imageSrc} alt="Image" width="100" height="100" />
+            <span>
+                {item.name}
+            </span>
+            <label>Gift wrap <input type="checkbox" checked={gift} onChange={(e) => setGift(e.target.checked)} /></label>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <button onClick={decreaseQuantity}>-</button>
+                <input //The input for quantity.
+                    type="number"
+                    value={quantity} //Input value bound to components quantity.
+                    onChange={(e) => setQuantity(Number(e.target.value) || 0)} //Update quantity state if quantity is changed.
+                    style={{ textAlign: 'center', width: '30px', margin: '0 5px' }}
+                    min="1" //Set the minimun number to 1.
+                />
+                <button onClick={increaseQuantity}>+</button>
+            </div>
+            {" " + item.rebatePercent + "% discount for " + item.rebateQuantity + "pcs, "} {/*Button to remove the item.*/}
+            <span>
+                {" " + subTotal.toFixed(2)} {/*Display the subtotal.*/}
+            </span>
+            {"( " + totalDiscount.toFixed(2) + ")"} {/*Display the subtotal.*/}
+            
+                <Button onClick={() => onRemove(item.id)}>🗑️</Button> {/*Button to remove the item.*/}
+            
+        </div>
+    )
+}
 
 export default ItemComponent; //Export the component.
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,26 +1,26 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ItemComponent from "./ItemComponent";
 import { Item } from '../types/Items';
-import Table from 'react-bootstrap/Table';
-import image1 from '/Users/lynguyenhansen/JavaScript/02363FrontEndProject/src/assets/images/d3-vitamin.jpeg';
-import image2 from '/Users/lynguyenhansen/JavaScript/02363FrontEndProject/src/assets/images/c-vitamin-500.jpeg';
-import image3 from '/Users/lynguyenhansen/JavaScript/02363FrontEndProject/src/assets/images/c-vitamin-depot.jpeg';
-import image4 from '/Users/lynguyenhansen/JavaScript/02363FrontEndProject/src/assets/images/omega.jpeg';
+import { Table } from 'react-bootstrap';
+import image1 from '../assets/images/d3-vitamin.jpeg';
+import image2 from '../assets/images/c-vitamin-500.jpeg';
+import image3 from '../assets/images/c-vitamin-depot.jpeg';
+import image4 from '../assets/images/omega.jpeg';
 
 
 /**
  * This is the hardcoded list of initial items.
  */
 const initItems: Item[] = [
-    { id: "vitamin-d-90-100", name: "D-vitamin, 90ug, 100 stk", type:"D-vitamin", imageSrc:image1, price: 116, amount: 0, rebateQuantity: 3, rebatePercent: 10, gift: 0},
-    { id: "vitamin-c-500-250", name: "C-vitamin, 500mg, 240 stk", type:"C-vitamin",imageSrc:image2, amount: 0, rebateQuantity: 3, rebatePercent: 10, price: 175, gift: 0},
-    { id: "vitamin-c-depot-500-250", name: "C-vitamin Depot, 500mg, 240 stk",type:"C-vitamin",imageSrc:image3, amount: 0, rebateQuantity: 3, rebatePercent: 10, price: 175, gift: 0},
-    { id: "fish-oil-1000-120", name: "Omega 3 fiskeolie, 1000mg, 120 stk", type:"Omega",imageSrc:image4, amount: 0, rebateQuantity: 3, rebatePercent: 10, price: 175, gift: 0}
+    { id: "vitamin-d-90-100", name: "D-vitamin, 90ug, 100 stk", type: "D-vitamin", imageSrc: image1, price: 116, amount: 0, rebateQuantity: 3, rebatePercent: 10, gift: 0 },
+    { id: "vitamin-c-500-250", name: "C-vitamin, 500mg, 240 stk", type: "C-vitamin", imageSrc: image2, amount: 0, rebateQuantity: 3, rebatePercent: 10, price: 175, gift: 0 },
+    { id: "vitamin-c-depot-500-250", name: "C-vitamin Depot, 500mg, 240 stk", type: "C-vitamin", imageSrc: image3, amount: 0, rebateQuantity: 3, rebatePercent: 10, price: 175, gift: 0 },
+    { id: "fish-oil-1000-120", name: "Omega 3 fiskeolie, 1000mg, 120 stk", type: "Omega", imageSrc: image4, amount: 0, rebateQuantity: 3, rebatePercent: 10, price: 175, gift: 0 }
 ];
 
 const ItemList: React.FC = () => {
     //Hook for the list of items.
-    const[items, setItems] = useState<Item[]>(initItems);
+    const [items, setItems] = useState<Item[]>(initItems);
     //Hook for tracking the subtotal for each item based on its quantity.
     const [subtotals, setSubtotals] = useState<{ [key: string]: number }>({});
 
@@ -34,8 +34,8 @@ const ItemList: React.FC = () => {
     }, []);
 
     //Function to removes an item.
-    const removeItem = (id: string) =>{
-        setItems(items.filter(item=>item.id!==id));
+    const removeItem = (id: string) => {
+        setItems(items.filter(item => item.id !== id));
         const newSubtotals = { ...subtotals };
         delete newSubtotals[id];
         setSubtotals(newSubtotals);
@@ -50,34 +50,35 @@ const ItemList: React.FC = () => {
     //The total price for all items
     let total = Object.values(subtotals).reduce((acc, curr) => acc + curr, 0);
     let discount = 0;
-    if(total > 300){
+    if (total > 300) {
         discount = total;
         total = total * 0.90;
         discount = discount - total;
     }
 
     //Maps each item to an itemComponent and display the total price.
-    return(
-        <Table striped bordered hover>
-        <thead>
-            <title>Indkøbskurv </title>
-            <tr>
-                <th>Product</th>
-                <th></th>
-                <th>Quantity</th>
-                <th>Subtotal</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-        
-            {items.map(item=>(
-                <ItemComponent key={item.id} item={item} onRemove={removeItem} onQuantityChange={handleQuantityChange} />
-            ))}
+    return (
+        <>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Product</th>
+                        <th></th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map(item => (
+                        <ItemComponent key={item.id} item={item} onRemove={removeItem} onQuantityChange={handleQuantityChange} />
+                    ))}
+
+                </tbody>
+            </Table>
             <div>Total: ${total.toFixed(2)}</div>
             <div>Total discount: ${discount.toFixed(2)}</div>
-        </tbody>
-        </Table>
+        </>
     )
 }
 
