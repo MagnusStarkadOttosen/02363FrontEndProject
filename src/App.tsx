14b8a5dfa79be7a14eb46458dbbeec086295cee1
Magnus Ottosen <magnus@ottosen.dk>
2024-04-08 13:48:41 +0200

    Made it work

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import "./App.css";
 import ItemList from "./components/ItemList";
-import BillingAndDelivery from "./components/BillingAndDelivery";
+import BillingAndDelivery from "./components/billingAndDelivery";
 import DisplayComponent from "./components/DisplayComponent";
 
 function App() {
   return (
-    <DisplayComponent />
+    //<DisplayComponent />
 
-    // <div className="row">
+    <div className="row">
 
-    //   <div className="ItemListArea">
-    //     <ItemList />
-    //   </div>
+      <div className="ItemListArea">
+        <ItemList />
+      </div>
 
-    //   <div className="InfoArea">
-    //     {/*
-    //     <div className="TotalArea">
-    //       placeholder for total price
-    //     </div>
-    //     */}
-    //     <div className="BillingAndDeliveryArea">
-    //       <BillingAndDelivery />
-    //     </div>
+      <div className="InfoArea">
+        {/*
+        <div className="TotalArea">
+          placeholder for total price
+        </div>
+        */}
+        <div className="BillingAndDeliveryArea">
+          <BillingAndDelivery />
+        </div>
 
-    //   </div>
+      </div>
 
-    // </div>
+    </div>
   );
 }
 
